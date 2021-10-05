31a9be5ff0d60a4464dd1cef4cde63de70e6428f
Duy Truong <zeref278@gmail.com>
2021-10-05 16:42:59 +0700

    Create flutter project

---
+++ lettutor/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('lettutor'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
