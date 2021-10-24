d0b9f15ff0ee488a495726a2f580abd77b7d9054
Duy Truong <zeref278@gmail.com>
2021-10-24 23:17:00 +0700

    Rename and clean something

--- lettutor/lib/constants/ui_constants.dart
+++ lettutor/lib/constants/ui_constants.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 
-
+//------Constant color
+///Color of social sign in button
 const Color facebookColor =  Color(0xff39579A);
 const Color githubColor =  Color(0xff202020);
 const Color googleColor =  Color(0xffDF4A32);
@@ -11,7 +12,7 @@ const Color lightColor = Color(0xFF577bff);
 const Color defaultColor = Color(0xFFffde59);
 
 const Color darkYellow = Color(0xFFffc45d);
-//const Color defaultColor = Color(0xFFffc45d);
+
 const Color backgroundFieldColor = Color(0xFFe0e9ff);
 
 const Color bgColor = Colors.white;
